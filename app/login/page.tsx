760ef2f59275b737d2e7ca931907637db1b48e45
Matheus Braz <suehtamzarb@gmail.com>
2025-11-10 18:39:22 -0300

    feat: create signup form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,14 @@
 export default function Login() {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="min-h-screen flex items-center justify-center">
+            <form className="p-8 rounded-xl bg-linear-to-br from-primary to-secondary flex flex-col gap-4 items-center justify-center">
+                <h1 className="text-2xl font-bold">Sign Up</h1>
+                <label>Email</label>
+                <input className="border p-2 rounded bg-transparent" placeholder="Email" type="email" />
+                <label>Password</label>
+                <input className="border p-2 rounded bg-transparent" placeholder="Password" type="password" />
+                <button className="bg-primary text-white p-2 rounded" type="submit" >Login</button>
+            </form>
         </div>
     )
 }
